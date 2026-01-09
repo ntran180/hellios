56f446098bdb29337ec36883c4c0fe294b95cf94
ntran180 <ntran180@uw.edu>
2026-01-09 01:13:42 -0800

    First push

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nam Tran on 1/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by Nam Tran on 1/9/26.
+//
+
+import Testing
+@testable import HelloWorld
+
+struct HelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
